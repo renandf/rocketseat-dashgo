4ef9c62e8ca9257f75ce9a50f584d424b1729f25
Renan Castro <renandf@gmail.com>
2021-07-04 19:08:20 +1000

    feat: Create UI for SignIn form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Flex, Stack, Button } from '@chakra-ui/react';
+import { Input } from '../components/Form/Input';
 
-export default function Home() {
+export default function SignIn() {
   return (
-    <h1>Dashgo</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" type="email" label="Email" />
+          <Input name="password" type="password" label="Password" />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">Enter</Button>
+
+      </Flex>
+    </Flex>
   )
 }
