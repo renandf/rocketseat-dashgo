d5e2457441209a51540374faa486d26927d33d29
Renan Castro <renandf@gmail.com>
2021-07-09 07:39:33 +1000

    feat: Make Sidebar responsive as drawer on mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
 
 import { Logo } from './Logo';
 import { SearchBox } from './SearchBox';
 import { NotificationsNav } from './NotificationsNav';
 import { Profile } from './Profile';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
+import { RiMenuLine } from 'react-icons/ri';
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isLargeScreen = useBreakpointValue({
     base: false,
     lg: true
@@ -22,6 +26,19 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isLargeScreen && (
+        <IconButton
+          aria-label="Open navigation menu"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        >
+
+        </IconButton>
+      )}
+
       <Logo />
 
       {isLargeScreen && <SearchBox />}
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,23 +1,38 @@
-import { Box, Stack } from '@chakra-ui/react';
-import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from 'react-icons/ri';
+import { useBreakpointValue, Box, Drawer, DrawerCloseButton, DrawerContent, DrawerOverlay, DrawerHeader, DrawerBody } from '@chakra-ui/react';
 
-import { NavLink } from './NavLink';
-import { NavSection } from './NavSection';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
+
+import { SidebarNav } from './SidebarNav';
 
 export function Sidebar() {
+  const { isOpen, onClose } = useSidebarDrawer();
+
+  const isDrawerSidebar = useBreakpointValue({
+    base: true,
+    lg: false
+  })
+
+  if (isDrawerSidebar) {
+
+    return (
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay>
+          <DrawerContent bg="gray.800" p="4">
+            <DrawerCloseButton mt="6" />
+            <DrawerHeader>Menu</DrawerHeader>
+
+            <DrawerBody>
+              <SidebarNav />
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    )
+  }
+
   return (
     <Box as="aside" w="64" mr="8">
-      <Stack spacing="12" align="flex-start">
-        <NavSection title="GENERAL">
-          <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
-          <NavLink icon={RiContactsLine}>Users</NavLink>
-        </NavSection>
-
-        <NavSection title="AUTOMATION">
-          <NavLink icon={RiInputMethodLine}>Forms</NavLink>
-          <NavLink icon={RiGitMergeLine}>Automation</NavLink>
-        </NavSection>
-      </Stack>
+      <SidebarNav />
     </Box>
   )
 }
