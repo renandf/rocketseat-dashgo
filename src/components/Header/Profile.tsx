4a22fbb313feca85007d3fe429ed66750f968d3e
Renan Castro <renandf@gmail.com>
2021-07-08 21:05:22 +1000

    feat: Make Header responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileDetails?: boolean;
+}
+
+export function Profile({ showProfileDetails = true }: ProfileProps) {
   return (
-    <Flex
-      align="center"
-    >
-      <Box mr="4" textAlign="right">
-        <Text>Diego Fernandes</Text>
-        <Text color="gray.300" fontSize="small">
-          diego.shell@email.com
-        </Text>
-      </Box>
+    <Flex align="center">
+      {showProfileDetails && (
+        <Box mr="4" textAlign="right">
+          <Text>Diego Fernandes</Text>
+          <Text color="gray.300" fontSize="small">
+            diego.shell@email.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
